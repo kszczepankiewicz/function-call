2c33f575c311097f36627a68172464bb0d3a761e
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-17 04:33:27 +0200

    feat: add structure

---
+++ script.js
@@ -0,0 +1,4 @@
+const functionCallForm = document.getElementById('function-call-form');
+const functionCallNameInput = document.getElementById('function-call-name-input');
+const functionCallParametersInput = document.getElementById('function-call-parameters-input');
+const result = document.getElementById('result');
