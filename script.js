468271ee66784b4e0ae1bbebbe5b9824d7c06ed5
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-17 04:48:35 +0200

    fix(index.html): add function-call-parameters-input id to input

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const functionCallForm = document.getElementById('function-call-form');
+const result = document.getElementById('result');
+
 const functionCallNameInput = document.getElementById('function-call-name-input');
 const functionCallParametersInput = document.getElementById('function-call-parameters-input');
-const result = document.getElementById('result');
+
+function generateFunctionCall(functionName, functionParameters) {
+    if (!functionName) {
+        alert('Empty name');
+        return '';
+    }
+    return `//${functionName}(${functionParameters})`;
+}
+
+functionCallForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    debugger
+    result.textContent = generateFunctionCall(functionCallNameInput.value, functionCallParametersInput.value)
+})
